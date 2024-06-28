ab500a6b3971654952224a93e93595fbd8cdfe5d
Ziyi Zhang <159777538+z2004y@users.noreply.github.com>
2024-06-29 01:31:36 +0800

    scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function showSkill(skillId) {
     var details = document.getElementById('skillDetails');
     switch(skillId) {
         case 'skill1':
-            details.innerHTML = '<p>技能1的详细描述。</p>';
+            details.innerHTML = '<p>技能1的干活好几年你不敢好吧好吧一把浴霸眼巴巴描述。</p>';
             break;
         case 'skill2':
             details.innerHTML = '<p>技能2的详细描述。</p>';
