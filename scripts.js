47a974eccfb05f1886fa0da689e99d2ffe5d3b64
Ziyi Zhang <159777538+z2004y@users.noreply.github.com>
2024-06-29 01:34:03 +0800

    scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function showSkill(skillId) {
             details.innerHTML = '<p>技能1的干活好几年你不敢好吧好吧一把浴霸眼巴巴描述。</p>';
             break;
         case 'skill2':
-            details.innerHTML = '<p>技能2的详细描述。</p>';
+            details.innerHTML = '<p>技能2的详细         有哈哈哈哈哈哈哈哈哈描述。</p>';
             break;
         // 更多技能
     }
